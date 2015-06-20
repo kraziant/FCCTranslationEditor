b30941ad35e6ef1b73f84fbc43aac7f680f063c4
Kuzyakin Anton <kraziant@ya.ru>
2015-06-20 23:52:14 +0300

    backend server

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var fs = require('fs');
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+var exec = require('child_process').exec;
+
+
+var config = {
+  "port": 3000,
+  "target": "/home/anton/work/FCC-Russian-Translation/basic-html5-and-css.json"
+};
+
+app.use(bodyParser.json({limit: '5mb'}));
+//app.use(bodyParser.urlencoded({limit: '5mb', extended:true}));
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+
 
 app.get('/', function (req, res) {
   fs.readFile('index.html', 'utf8', function (err, data) {
@@ -11,7 +26,29 @@ app.get('/', function (req, res) {
   });
 });
 
-var server = app.listen(3000, function () {
+app.get('/get_data', function (req, res) {
+  fs.readFile(config.target, 'utf8', function (err, data) {
+    if (err) {
+      res.send( err );
+      return console.log(err);
+    }
+    res.send( data );
+  });
+});
+
+app.post('/post_data', function(req, res) {
+    console.log('ok');
+    fs.writeFile('tmp.txt', JSON.stringify(req.body, null, 2), 'utf-8', function () {
+      console.log('data saved');
+      exec("patch < difficult.patch", function() {
+        console.log('difficult.patch apply to saved file');
+        exec("cp tmp.txt " + config.target, function(){});
+      });
+    });
+    res.end();
+});
+
+var server = app.listen(config.port, function () {
 
   var host = server.address().address;
   var port = server.address().port;
